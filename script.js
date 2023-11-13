878187b3cc9cfbcbc0cdccac42918a8e008460e4
lam <i.mailed.lam@gmail.com>
2023-11-13 15:00:49 +0200

    CElcius to ferenheight

---
+++ script.js
@@ -0,0 +1,7 @@
+var tempCelsius = 10;
+var toFahrenheit = tempCelsius * 1.8 + 32;
+
+console.log(
+  `outside my friends house its ${tempCelsius} degrees celsius, which is ${toFahrenheit} degrees fahrenheit.`
+);
+
